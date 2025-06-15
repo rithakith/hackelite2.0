903b031aa0d36ab22e7dd1ce66f7dd355c57bd10
rithakith <ritharaedirisinghe@gmail.com>
2025-06-15 07:57:26 +0530

    changes

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -10,8 +10,8 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mb-12">
           {" "}
           {/* Brand Section */}
-          <div className="space-y-4 flex flex-col items-center md:items-start">
-            <div className="relative h-24 w-80 max-w-full">
+          <div className="space-y-4  lg:items-left flex flex-col items-center ">
+            <div className="relative h-24 w-80 max-w-full lg:max-w-none">
               <Image
                 src="/hackelite_logo.png"
                 alt="HackElite 2.0"
@@ -19,7 +19,7 @@ const Footer = () => {
                 className="object-contain"
               />
             </div>
-            <p className="text-white/60 text-sm text-center md:text-left">
+            <p className="text-white/60 text-sm text-center ">
               Transform your ideas into reality at the premier innovation
               challenge of the year
             </p>
